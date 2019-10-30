cdd4ca77739ee349a880cd2e9725c47aa3dc1684
soarflat <soarflat777@gmail.com>
2019-10-30 15:15:10 +0900

    chore: React アプリケーションのビルド環境を構築

---
+++ build/precache-manifest.fa4a8b58ab036d0d03abcf71cd753f19.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28c6c8ea51b6a6f9a1f60ad1bbfb976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6058d07b274440f5d469",
+    "url": "/static/js/2.47854bcd.chunk.js"
+  },
+  {
+    "revision": "96dd0e488ee41c4b4fd1",
+    "url": "/static/js/main.2ffb824a.chunk.js"
+  },
+  {
+    "revision": "d83e9c8bd7918819607f",
+    "url": "/static/js/runtime~main.08c1c9ef.js"
+  }
+]);
--- index.js
+++ index.js
@@ -36,6 +36,8 @@ let notes = [
   }
 ];
 
+app.use(express.static('build'));
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
